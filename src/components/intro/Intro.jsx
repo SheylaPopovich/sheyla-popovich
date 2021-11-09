82fff2eadf298f098c20e97c316fe3c03b3b945d
Sheyla Popovich <sheylapopovich@gmail.com>
2021-11-08 21:24:45 -0700

    added profile pic

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,12 @@ import "./intro.css"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-           
+           <div className="left">
+               <div className="imageContainer">
+                   <img src="public\assets\me.png" alt="woman"/>
+               </div>
+           </div>
+           <div className="right"></div>
         </div>
     )
 }
