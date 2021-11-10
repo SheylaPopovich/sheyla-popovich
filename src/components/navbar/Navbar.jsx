7fa676777579348c6d3ae0fdce9ff6a0c9c1a5e8
Sheyla Popovich <sheylapopovich@gmail.com>
2021-11-10 12:22:06 -0700

    Navbar fixed, links to correct sections

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,26 +3,36 @@ import "./navbar.css";
 export default function Navbar() {
   return (
     <div className="navbar">
-    <div className="wrapper">
-      <div className="left">
-        <a href="#intro" className="logo">
-          Pops.
+      <a href="#intro" className="logo">
+                Pops.
+      </a>
+      <a href="#works">
+      works
         </a>
-        <div className="itemContainer">
-          <span>+44 924 12 74</span>
-        </div>
-        <div className="itemContainer">
-          <span>safak@genius.com</span>
-        </div>
-      </div>
-      <div className="right">
-        <div className="hamburger">
-          <span className="line1"></span>
-          <span className="line2"></span>
-          <span className="line3"></span>
-        </div>
-      </div>
+        <a href="#portfolio">
+      portfolio
+        </a>
+        <a href="#contact">
+      contact me
+        </a>
     </div>
-  </div>
-  );
+  )
 }
+
+
+// <div className="wrapper">
+//       <div className="left">
+//         <a href="#intro" className="logo">
+//           Pops.
+//         </a>
+       
+       
+//       </div>
+//       <div className="right">
+//         <div className="hamburger">
+//           <span className="line1"></span>
+//           <span className="line2"></span>
+//           <span className="line3"></span>
+//         </div>
+//       </div>
+//     </div> 
