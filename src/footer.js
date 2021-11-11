6dbc668e7e77d86a5021328d39b1307a7cc40849
Sheyla Popovich <sheylapopovich@gmail.com>
2021-11-11 15:32:12 -0700

    footer icons linked to hrefs

--- src/footer.js
+++ src/footer.js
@@ -3,16 +3,16 @@ import React from "react";
 const Footer = () => (
     <footer className="footer">
       <p>This is react sticky footer!!</p>
-      <a class="btn btn-social-icon btn-github">
-    <span class="fa fa-github fa-2x "></span>
+      <a href="https://github.com/SheylaPopovich"class="btn btn-social-icon btn-github" target="_blank">
+    <span class="fa fa-github fa-2x fa-pulse "></span>
   </a>
 
-  <a class="btn btn-social-icon btn-instagram">
-    <span class="fa fa-instagram fa-2x"></span>
+  <a href="https://www.instagram.com/sheyla_popovich/" class="btn btn-social-icon btn-instagram" target="_blank">
+    <span class="fa fa-instagram fa-2x fa-pulse"></span>
   </a>
 
-  <a class="btn btn-social-icon btn-linkedin">
-    <span class="fa-2x fab fa-linkedin"></span>
+  <a href="https://www.linkedin.com/in/sheyla-popovich-fsd/"class="btn btn-social-icon btn-linkedin" target="_blank">
+    <span class="fa-2x fab fa-linkedin fa-pulse"></span>
   </a>
     </footer>
   );
